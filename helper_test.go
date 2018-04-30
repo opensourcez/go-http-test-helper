74f4321e0543000da6d32abbdfc5639a053f41a4
zkynet <anon@zkynet.io>
2018-04-30 18:50:18 +0300

    adding header test

--- helper_test.go
+++ helper_test.go
@@ -16,6 +16,10 @@ type Test struct {
 	Hello string `json:"hello"`
 }
 
+type Header struct {
+	ContentType string `json:"content_type"`
+}
+
 func TestMain(t *testing.T) {
 
 	fmt.Println("Server started on port: " + os.Getenv("Port"))
@@ -85,12 +89,71 @@ func runTests(t *testing.T) {
 			}), t)
 
 	})
+
+	// Could not find key ( goodbye ) in response body
+	// Key (hello) is not suppose to be in response
+	t.Run("HelloTestThatShouldFail", func(t *testing.T) {
+
+		headers := map[string]string{
+			"Content-Type": "application/json",
+		}
+
+		testInstance := TestHelper{
+			TestResultMap: make(map[string]map[string]*gabs.Container),
+			ShouldLog:     true,
+			Cookies:       make(map[string]*http.Cookie),
+		}
+
+		testInstance.TestThis(NewHTTPTest(
+			HTTPTestIn{
+				Label: "TestHelper", TestCode: "HELPER-003",
+				Body:    []byte(`{"hello":"hello back at you !"}`),
+				URL:     "http://localhost:3333/test",
+				Method:  "POST",
+				Headers: headers,
+			},
+			HTTPTestOut{Body: "", Code: 200, Status: "200 OK",
+				KeyValues: map[string]string{
+					"hello": "hello back at you !",
+				},
+				KeyPresent: []string{"goodbye"},
+			}), t)
+
+	})
+
+	t.Run("HeaderTest", func(t *testing.T) {
+
+		headers := map[string]string{
+			"Content-Type": "application/json",
+		}
+
+		testInstance := TestHelper{
+			TestResultMap: make(map[string]map[string]*gabs.Container),
+			ShouldLog:     true,
+			Cookies:       make(map[string]*http.Cookie),
+		}
+
+		testInstance.TestThis(NewHTTPTest(
+			HTTPTestIn{
+				Label: "TestHelper", TestCode: "HELPER-003",
+				Body:    []byte(`{"hello":"hello back at you !"}`),
+				URL:     "http://localhost:3333/reflect-header",
+				Method:  "POST",
+				Headers: headers,
+			},
+			HTTPTestOut{Body: "", Code: 200, Status: "200 OK",
+				KeyValues: map[string]string{
+					"content_type": "application/json",
+				},
+				KeyPresent: []string{"content_type"},
+			}), t)
+
+	})
 }
 
 func setUpRoutes() *mux.Router {
 	r := mux.NewRouter()
 
-	// USER
 	r.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 
 		var test Test
@@ -100,6 +163,19 @@ func setUpRoutes() *mux.Router {
 
 		w.WriteHeader(200)
 		if err := json.NewEncoder(w).Encode(test); err != nil {
+			panic(err)
+		}
+
+	}).Methods("POST")
+
+	r.HandleFunc("/reflect-header", func(w http.ResponseWriter, r *http.Request) {
+
+		header := Header{
+			ContentType: r.Header.Get("Content-Type"),
+		}
+
+		w.WriteHeader(200)
+		if err := json.NewEncoder(w).Encode(header); err != nil {
 			panic(err)
 		}
 
